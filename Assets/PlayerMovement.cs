04e63d5a63c2753c5833874a812fdb2d8edc9a1d
Adam Delfs <adam@Adams-Air.hitronhub.home>
2025-03-02 14:45:24 -0800

    added free look third person camera

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,24 +2,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller; 
-    public float speed = 5f; 
+    public CharacterController controller;
+    public Transform cameraTransform; 
+    public float speed = 5f;
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>(); 
+        controller = GetComponent<CharacterController>();
+
+       
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal"); 
-        float vertical = Input.GetAxis("Vertical"); 
+        if (controller == null) return;
 
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized; 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime); 
+            
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
